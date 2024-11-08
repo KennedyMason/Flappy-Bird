13a191136782ea06b098109f484defa3aa66c866
KennedyMason <kennedy.mason@ucas-edu.net>
2024-11-08 11:49:50 -0700

    bird flap

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import pygame
 import sys
-
+from game import Game
 
 pygame.init()
 screen = pygame.display.set_mode((400, 720))
 clock = pygame.time.Clock()
+game = Game("img/bird.png", "img/pipe.png", "img/background.png", "img/ground.png")
+game.resize()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game.active:
+                game.flap()
+
+    game.show_background(screen)
+    
+    if game.active:
+        game.show_bird(screen)
+        game.update_bird()
+
+    game.show_ground(screen)
+    game.move_ground()
+
+
 
     pygame.display.update()
     clock.tick(120)
